9887de9e52b6c3fdc9d65e585a73601b5b44ed77
XiangAo11 <143826598+XiangAo11@users.noreply.github.com>
2024-03-27 20:11:57 -0400

    XiangAo lab commit

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,9 @@
-# J Dickens  James Dickens
-
+# Xiang Ao
+# The data source is the mpg data table
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+ggplot(mpg, aes(x = factor(1), y = displ)) + 
+  geom_boxplot(fill = "blue") +
+  labs(x = "", y = "Displacement (L)") +
+  coord_flip()
